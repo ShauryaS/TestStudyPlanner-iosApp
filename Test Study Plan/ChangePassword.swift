e76bb4e49ec7541fc9f2e8b0d865b9e5fc19573a
Shaurya Srivastava <shauryas481@gmail.com>
2016-04-11 01:26:23 -0700

    Added screen to change password. Enabled 3D touch functionality. Add icon for app.

---
+++ Test Study Plan/ChangePassword.swift
@@ -0,0 +1,9 @@
+//
+//  ChangePassword.swift
+//  Test Study Plan
+//
+//  Created by Shaurya Srivastava on 4/11/16.
+//  Copyright © 2016 Shaurya Srivastava. All rights reserved.
+//
+
+import Foundation
