0640f16f2dcd9da836e88c0564698c0f5cdcf276
Shaurya Srivastava <shauryas481@gmail.com>
2016-04-16 00:42:27 -0700

    Replaed lables of data received with buttons

--- Test Study Plan/PlansView.swift
+++ Test Study Plan/PlansView.swift
@@ -31,13 +31,12 @@ class PlansView: UIViewController{
         var ref = Firebase(url: "https://test-study-plan-ios.firebaseio.com/Users")
         ref = ref.childByAppendingPath(ref.authData.uid).childByAppendingPath("Plans")
         ref.queryOrderedByKey().observeEventType(.ChildAdded, withBlock: {snapshot in
-            /*let button = UIButton(frame: CGRectMake(0,(CGFloat(self.i)-1)*25,374,25))
-            button.titleLabel!.text = snapshot.key
-            button.titleLabel!.font = button.titleLabel!.font.fontWithSize(20)*/
-            let lab = UILabel(frame: CGRectMake(0,(CGFloat(self.i)-1)*25,374,25))
-            lab.font = lab.font.fontWithSize(20)
-            lab.text = snapshot.key
-            self.plansList.addSubview(lab)
+            let button = UIButton(frame: CGRectMake(0,(CGFloat(self.i)-1)*25,450,25))
+            button.titleLabel!.font = button.titleLabel!.font.fontWithSize(25)
+            button.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+            button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            button.setTitle(snapshot.key, forState: .Normal)
+            self.plansList.addSubview(button)
             if (CGFloat(self.i))*25>self.plansList.contentSize.height{
                 self.plansList.contentSize.height+=25
             }
